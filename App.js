ee7692c1642511f8592b6213779362b53778538d
Neelesh Singh <neelesh@Neeleshs-MacBook-Air.local>
2023-03-19 00:13:06 +0530

    Added native android changes for object detection

--- App.js
+++ App.js
@@ -1,41 +1,46 @@
-import React, { useEffect, useState, useMemo, } from 'react'
-import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native'
-import { Camera, useCameraDevices, useFrameProcessor, } from 'react-native-vision-camera';
+import React, {useEffect, useState, useMemo} from 'react';
+import {StyleSheet, Text, View, PermissionsAndroid} from 'react-native';
+import {
+  Camera,
+  useCameraDevices,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
 import scanQR from './plugin';
-import { runOnJS } from 'react-native-reanimated';
-import { Canvas, RoundedRect, useValue, useImage, Image } from "@shopify/react-native-skia";
-
+import {runOnJS} from 'react-native-reanimated';
+import {
+  Canvas,
+  RoundedRect,
+  useValue,
+  useImage,
+  Image,
+} from '@shopify/react-native-skia';
 
 export default function App() {
+  const devices = useCameraDevices();
+  const device = devices.back;
 
-  const devices = useCameraDevices()
-  const device = devices.back
-
-  const [cameraPermission, setCameraPermission] = useState(true)
-  const [microphonePermission, setMicrophonePermission] = useState(true)
-  const [screenHeight, setScreenHeight] = useState(0)
-  const [screenWidth, setScreenWidth] = useState(0)
-  const [faces, setFaces] = useState([])
+  const [cameraPermission, setCameraPermission] = useState(true);
+  const [microphonePermission, setMicrophonePermission] = useState(true);
+  const [screenHeight, setScreenHeight] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(0);
+  const [faces, setFaces] = useState([]);
   const skia_faces = useValue([]);
-  const [latency, setLatency] = useState(0)
+  const [latency, setLatency] = useState(0);
 
   useEffect(() => {
-
     async function perm() {
+      await Camera.requestCameraPermission();
+      await Camera.requestMicrophonePermission();
 
-      await Camera.requestCameraPermission()
-      await Camera.requestMicrophonePermission()
+      let cameraPerm = await Camera.getCameraPermissionStatus();
+      let microphonePerm = await Camera.getMicrophonePermissionStatus();
 
-      let cameraPerm = await Camera.getCameraPermissionStatus()
-      let microphonePerm = await Camera.getMicrophonePermissionStatus()
-
-      setCameraPermission(cameraPerm === 'authorized')
-      setMicrophonePermission(microphonePerm === 'authorized')
+      setCameraPermission(cameraPerm === 'authorized');
+      setMicrophonePermission(microphonePerm === 'authorized');
     }
 
-    perm()
-
-  }, [])
+    perm();
+  }, []);
 
   const format = useMemo(() => {
     if (device) {
@@ -43,36 +48,38 @@ export default function App() {
         if (device?.formats[i].photoWidth && device?.formats[i].photoHeight) {
           if (device?.formats[i].photoHeight === 480) {
             // console.log("&&&&", device?.formats[i])
-            return device?.formats[i]
+            return device?.formats[i];
           }
         }
       }
     }
-  }, [device?.formats])
+  }, [device?.formats]);
 
-  const frameProcessor = useFrameProcessor((frame) => {
-    'worklet'
+  const frameProcessor = useFrameProcessor(
+    frame => {
+      'worklet';
 
-    let result = scanQR(frame)
-    // to se state
-    // runOnJS(setFaces)(result) -> where setFaces is a state setter
-    console.log("***", result)
-
-  }, [screenHeight, screenWidth])
+      let result = scanQR(frame);
+      // to se state
+      // runOnJS(setFaces)(result) -> where setFaces is a state setter
+      console.log('***', result);
+    },
+    [screenHeight, screenWidth],
+  );
 
   useEffect(() => {
     skia_faces.current = faces;
-  }, [faces])
+  }, [faces]);
 
   if (device && cameraPermission && microphonePermission) {
     return (
       <View style={styles.container}>
         <Camera
-          onLayout={(e) => {
-            setScreenHeight(e.nativeEvent.layout.height)
-            setScreenWidth(e.nativeEvent.layout.width)
+          onLayout={e => {
+            setScreenHeight(e.nativeEvent.layout.height);
+            setScreenWidth(e.nativeEvent.layout.width);
           }}
-          style={{ height: '100%', width: "100%" }}
+          style={{height: '100%', width: '100%'}}
           device={device}
           isActive={true}
           format={format}
@@ -80,18 +87,14 @@ export default function App() {
           frameProcessor={frameProcessor}
         />
       </View>
-    )
+    );
   }
 
-  return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>
-    </View>
-  )
-
+  return <View style={{flex: 1, backgroundColor: 'white'}}></View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-})
+});
--- android/app/src/main/java/com/vision/QRPlugin.java
+++ android/app/src/main/java/com/vision/QRPlugin.java
@@ -30,6 +30,9 @@ import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
 import org.jetbrains.annotations.NotNull;
 import org.pytorch.IValue;
 import org.pytorch.LiteModuleLoader;
@@ -63,15 +66,247 @@ import android.os.Environment;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 
+class AnalysisResult {
+  private final ArrayList<Result> mResults;
+
+  public AnalysisResult(ArrayList<Result> results) {
+      this.mResults = results;
+  }
+}
+
+class Result {
+  Float score;
+  String className;
+  int left;
+  int right;
+  int top;
+  int bottom;
+
+  public Result(Float output, int left, int right, int top, int bottom, String className) {
+      this.score = output;
+      this.className = className;
+      this.left = left;
+      this.right = right;
+      this.top = top;
+      this.bottom = bottom;
+  }
+};
+
+class IntermediateResult {
+    int classIndex;
+    Float score;
+    Rect rect;
+
+    public IntermediateResult(int cls, Float output, Rect rect) {
+        this.classIndex = cls;
+        this.score = output;
+        this.rect = rect;
+    }
+};
+
 public class QRPlugin extends FrameProcessorPlugin {
+  // model input image size
+  static int mInputWidth = 640;
+  static int mInputHeight = 640;
 
+  // for yolov5 model, no need to apply MEAN and STD
+  static float[] NO_MEAN_RGB = new float[] {0.0f, 0.0f, 0.0f};
+  static float[] NO_STD_RGB = new float[] {1.0f, 1.0f, 1.0f};
 
+  // model output is of size 25200*(num_of_class+5)
+  private static int mOutputRow = 25200; // as decided by the YOLOv5 model for input image of size 640*640
+  private static int mOutputColumn = 85; // left, top, right, bottom, score and 80 class probability
+  private static float mThreshold = 0.30f; // score above which a detection is generated
+  private static int mNmsLimit = 15;
+
+  private Context mContext;
+  private Module mModule;
+
+  static String[] mClasses;
+
+  static float IOU(Rect a, Rect b) {
+    float areaA = (a.right - a.left) * (a.bottom - a.top);
+    if (areaA <= 0.0) return 0.0f;
+
+    float areaB = (b.right - b.left) * (b.bottom - b.top);
+    if (areaB <= 0.0) return 0.0f;
+
+    float intersectionMinX = Math.max(a.left, b.left);
+    float intersectionMinY = Math.max(a.top, b.top);
+    float intersectionMaxX = Math.min(a.right, b.right);
+    float intersectionMaxY = Math.min(a.bottom, b.bottom);
+    float intersectionArea = Math.max(intersectionMaxY - intersectionMinY, 0) *
+            Math.max(intersectionMaxX - intersectionMinX, 0);
+    return intersectionArea / (areaA + areaB - intersectionArea);
+  }
+
+  static ArrayList<Result> nonMaxSuppression(ArrayList<IntermediateResult> boxes, int limit, float threshold) {
+    Collections.sort(boxes,
+    new Comparator<IntermediateResult>() {
+        @Override
+        public int compare(IntermediateResult o1, IntermediateResult o2) {
+            return o1.score.compareTo(o2.score);
+        }
+    });
+
+    ArrayList<IntermediateResult> selected = new ArrayList<>();
+    boolean[] active = new boolean[boxes.size()];
+    Arrays.fill(active, true);
+    int numActive = active.length;
+
+    boolean done = false;
+    for (int i=0; i<boxes.size() && !done; i++) {
+      if (active[i]) {
+        IntermediateResult boxA = boxes.get(i);
+        selected.add(boxA);
+        if (selected.size() >= limit) break;
+
+        for (int j=i+1; j<boxes.size(); j++) {
+          if (active[j]) {
+            IntermediateResult boxB = boxes.get(j);
+            if (IOU(boxA.rect, boxB.rect) > threshold) {
+              active[j] = false;
+              numActive -= 1;
+              if (numActive <= 0) {
+                done = true;
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    ArrayList<Result> finalResult = new ArrayList<>();
+
+    for(int i=0; i<selected.size(); i++)
+    {
+      IntermediateResult intermediateResult = selected.get(i);
+      Result result = new Result(intermediateResult.score, intermediateResult.rect.left, 
+        intermediateResult.rect.right, intermediateResult.rect.top, intermediateResult.rect.bottom, 
+        mClasses[intermediateResult.classIndex]);
+        finalResult.add(result);
+    }
+
+    return finalResult;
+  }
+
+  static ArrayList<Result> outputsToNMSPredictions(float[] outputs, float imgScaleX, float imgScaleY, float ivScaleX, float ivScaleY, float startX, float startY) {
+    ArrayList<IntermediateResult> results = new ArrayList<>();
+    for (int i = 0; i< mOutputRow; i++) {
+      if (outputs[i* mOutputColumn +4] > mThreshold) {
+        float x = outputs[i* mOutputColumn];
+        float y = outputs[i* mOutputColumn +1];
+        float w = outputs[i* mOutputColumn +2];
+        float h = outputs[i* mOutputColumn +3];
+
+        float left = imgScaleX * (x - w/2);
+        float top = imgScaleY * (y - h/2);
+        float right = imgScaleX * (x + w/2);
+        float bottom = imgScaleY * (y + h/2);
+
+        float max = outputs[i* mOutputColumn +5];
+        int cls = 0;
+        for (int j = 0; j < mOutputColumn -5; j++) {
+          if (outputs[i* mOutputColumn +5+j] > max) {
+            max = outputs[i* mOutputColumn +5+j];
+            cls = j;
+          }
+        }
+
+        Rect rect = new Rect((int)(startX+ivScaleX*left), (int)(startY+top*ivScaleY), (int)(startX+ivScaleX*right), (int)(startY+ivScaleY*bottom));
+        IntermediateResult result = new IntermediateResult(cls, outputs[i*mOutputColumn+4], rect);
+        results.add(result);
+      }
+    }
+    return nonMaxSuppression(results, mNmsLimit, mThreshold);
+  }
+
+  private Bitmap imgToBitmap(Image image) {
+    Image.Plane[] planes = image.getPlanes();
+    ByteBuffer yBuffer = planes[0].getBuffer();
+    ByteBuffer uBuffer = planes[1].getBuffer();
+    ByteBuffer vBuffer = planes[2].getBuffer();
+
+    int ySize = yBuffer.remaining();
+    int uSize = uBuffer.remaining();
+    int vSize = vBuffer.remaining();
+
+    byte[] nv21 = new byte[ySize + uSize + vSize];
+    yBuffer.get(nv21, 0, ySize);
+    vBuffer.get(nv21, ySize, vSize);
+    uBuffer.get(nv21, ySize + vSize, uSize);
+
+    YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+    byte[] imageBytes = out.toByteArray();
+    return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+  }
+
+  private AnalysisResult analyzeImage(ImageProxy image) {
+    Bitmap bitmap = imgToBitmap(image.getImage());    
+    Matrix matrix = new Matrix();
+    matrix.postRotate(90.0f);
+    bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, mInputWidth, mInputHeight, true);
+    final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(resizedBitmap, NO_MEAN_RGB, NO_STD_RGB);
+    IValue[] outputTuple = mModule.forward(IValue.from(inputTensor)).toTuple();
+    final Tensor outputTensor = outputTuple[0].toTensor();
+    final float[] outputs = outputTensor.getDataAsFloatArray();
+
+    float imgScaleX = (float)bitmap.getWidth() / mInputWidth;
+    float imgScaleY = (float)bitmap.getHeight() / mInputHeight;
+    float ivScaleX = 1;
+    float ivScaleY = 1;
+
+    final ArrayList<Result> results = outputsToNMSPredictions(outputs, imgScaleX, imgScaleY, ivScaleX, ivScaleY, 0, 0);
+    return new AnalysisResult(results);
+  }
+
+  public static String assetFilePath(Context context, String assetName) throws IOException {
+    File file = new File(context.getFilesDir(), assetName);
+    if (file.exists() && file.length() > 0) {
+      return file.getAbsolutePath();
+    }
+
+    try (InputStream is = context.getAssets().open(assetName)) {
+      try (OutputStream os = new FileOutputStream(file)) {
+        byte[] buffer = new byte[4 * 1024];
+        int read;
+        while ((read = is.read(buffer)) != -1) {
+          os.write(buffer, 0, read);
+        }
+        os.flush();
+      }
+      return file.getAbsolutePath();
+    }
+  }
+  
   Interpreter.Options options = new Interpreter.Options();
   @Override
   public Object callback(ImageProxy image, Object[] params) {
+    try {
+      if (mModule == null) {
+          mModule = LiteModuleLoader.load(assetFilePath(mContext, "yolov5s.torchscript.ptl"));
+      }
+      BufferedReader br = new BufferedReader(new InputStreamReader(mContext.getAssets().open("classes.txt")));
+      String line;
+      List<String> classes = new ArrayList<>();
+      while ((line = br.readLine()) != null) {
+          classes.add(line);
+      }
+      mClasses = new String[classes.size()];
+      classes.toArray(mClasses);
+    } catch (IOException e) {
+      Log.e("Object Detection", "Error reading assets", e);
+    }
 
-    return "hey";
-
+    AnalysisResult result = analyzeImage(image);
+    Gson gson = new Gson();
+    String jsonString = gson.toJson(result);
+    return jsonString;
   } 
 
   QRPlugin(ReactApplicationContext context) {
